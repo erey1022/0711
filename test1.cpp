fc20b60447528aec806e3ec028a3547e5204c723
king1022 <king850822af@MS.com>
2025-07-11 14:08:09 +0800

    v2

--- test1.cpp
+++ test1.cpp
@@ -2,13 +2,13 @@
 int main(){
 
 
-    int change_money = 388 ;
-    int c =change_money/100;
-    int q=change_money%100/50;
-    int b=(change_money%100-50*q)/10;
+    int change_money = 378 ;
+    int hundred =change_money/100;
+    int fifty=change_money%100/50;
+    int tens=(change_money%100-50*fifty)/10;
     int a=change_money%10;
-    int s=a/5;
+    int five=a/5;
     a=a%5;
-    std::cout<<"找你"<<c<<"個100元"<<q<<"個50元"<<b<<"個10元"<<s<<"個5元"<<a<<"個1元";
+    std::cout<<"找你"<<hundred<<"個100元"<<fifty<<"個50元"<<tens<<"個10元"<<five<<"個5元"<<a<<"個1元";
     return 0;
 }
