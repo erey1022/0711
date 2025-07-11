4a9388fb5fbe96d86509c1437a52c49253ae628b
king1022 <king850822af@MS.com>
2025-07-11 18:09:11 +0800

    forloop

---
+++ for_loop.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+int main(){
+    int i;
+    int a[5]={12,56,85,45,55};
+    std::vector<int> b;
+    for(i = 0;i < 5; i+=1){
+        std::cout<<a[i]<<std::endl;
+        if(i%2==0){
+            b.push_back(a[i]);
+            b.push_back(a[i]+1);
+        }  else {
+            b.push_back(a[i]);
+            b.push_back(a[i]);
+        }
+    }
+    std::cout<<"----"<<std::endl;
+    for(i = 0;i <b.size(); i++){
+        std::cout<<b[i]<<std::endl;
+    }
+    return 0;
+}
--- test2.cpp
+++ test2.cpp
@@ -2,7 +2,23 @@
 
 int main(){
 
-    int score;
+    int i;
+    int a[5]={12,56,85,45,55};
+
+    for(i = 0;i < 5; i+=2){
+        std::cout<<a[i]<<std::endl;
+
+    }
+
+    std::cout<<"--------\n";
     
+    i=0;
+    std::cout<<++i;
+    std::cout<<" wait\n";
+    i=0;
+    std::cout<<"ready\n";
+    std::cout<<i++;
+    std::cout<<" end\n";
+    return 0;
 
 }
