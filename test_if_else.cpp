a7f3d7cb40d1ec614f02425163bc5e0241ff809f
king1022 <king850822af@MS.com>
2025-07-11 16:15:45 +0800

    scoreifel

--- test_if_else.cpp
+++ test_if_else.cpp
@@ -5,16 +5,23 @@ int main(){
     bool a = true;
     bool b = false;
 
-    int c = 3;
-    if(c == 3){
-        std::cout<<c<<std::endl;
-    } else{
-        std::cout<<"c is not 3"<<std::endl;
-    }
-    if(!a){
-        std::cout<<"this is true\n";
-    } else{
-        std::cout<<"this is false";
+    int c = 4;
+
+    bool x = (c==3);
+    int score = 0;
+
+    std::cout<<"please enter score: ";
+    std::cin>>score;//輸入
+    if(100 == score){
+        std::cout<<"A+"<<std::endl;
+    }  else if (score>=90){
+        std::cout<<"A"<<std::endl;
+    }  else if (score>=80){
+        std::cout<<"B"<<std::endl;
+    }  else if (score>=70){
+        std::cout<<"C"<<std::endl;
+    }  else if (score<70){
+        std::cout<<"F"<<std::endl;
     }
 
     return 0;
